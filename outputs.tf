96df96b26a53f6748f028872f5da59caf2b05f01
thakursuraj509 <61192405+thakursuraj509@users.noreply.github.com>
2023-05-25 10:56:44 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,37 @@
+output "replication_instance_arn" {
+  value = concat(aws_dms_replication_instance.ta_replication_instance.*.replication_instance_arn, [""])[0]
+}
+
+output "source_endpoint_arn" {
+  value = concat(aws_dms_endpoint.ta_source_endpoint.*.endpoint_arn, [""])[0]
+}
+
+output "target_endpoint_arn" {
+  value = concat(aws_dms_endpoint.ta_target_endpoint.*.endpoint_arn, [""])[0]
+}
+
+output "certificate_arn" {
+  value = concat(aws_dms_certificate.dms_certificate.*.certificate_arn, [""])[0]
+}
+
+output "replication_task_arn" {
+  value = concat(aws_dms_replication_task.ta_replication_task.*.replication_task_arn, [""])[0]
+}output "replication_instance_arn" {
+  value = concat(aws_dms_replication_instance.ta_replication_instance.*.replication_instance_arn, [""])[0]
+}
+
+output "source_endpoint_arn" {
+  value = concat(aws_dms_endpoint.ta_source_endpoint.*.endpoint_arn, [""])[0]
+}
+
+output "target_endpoint_arn" {
+  value = concat(aws_dms_endpoint.ta_target_endpoint.*.endpoint_arn, [""])[0]
+}
+
+output "certificate_arn" {
+  value = concat(aws_dms_certificate.dms_certificate.*.certificate_arn, [""])[0]
+}
+
+output "replication_task_arn" {
+  value = concat(aws_dms_replication_task.ta_replication_task.*.replication_task_arn, [""])[0]
+}
